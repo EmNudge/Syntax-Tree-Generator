f6fa49f4d82453a28afcaed4c1f3361f3b61ab4e
EmNudge <emnudge@gmail.com>
2020-05-28 01:58:36 -0400

    added service worker register

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import App from './App.svelte';
 import { setFromStorage } from './utils/storage';
 setFromStorage();
 
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/service-worker.js')
+		.then(() => console.log('Service Worker Registered'));
+}
+
 const app = new App({
 	target: document.body
 });
